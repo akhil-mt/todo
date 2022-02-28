bbf4efe6a1a7cf58f27ed0fdc9d3aee553473db7
Akhil Mathew Thomas <akhiltomez@gmail.com>
2022-02-28 22:23:46 +0530

    Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <todo-list>
+    </todo-list>
+  </div>
+</template>
+
+<script>
+import todoList from './components/todoList.vue'
+import './assets/tailwind.css'
+
+export default {
+  name: 'App',
+  components: {
+    todoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
