b3e950072fb8e25b4638d759ee8b339d3065418d
Akhil Mathew Thomas <akhiltomez@gmail.com>
2022-03-04 21:10:34 +0530

    after styling

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,29 +1,53 @@
 <template>
   <div>
     <section>
-      <h1 class="text-4xl">{{ title }}</h1>
-      <textarea
-        v-model="newTodo"
-        name=""
-        id=""
-        cols="60"
-        rows="5"
-        placeholder="Enter your task"
-      ></textarea
-      ><br />
-      <!-- <input v-model="newTodo" type="textarea" placeholder="Enter your task" name="newTodo" id="newTodo" ><br> -->
-      <button @click.prevent="addTodo">Submit</button>
-      <p>{{ newTodo }}</p>
+      <h1 class="text-2xl uppercase font-black mb-6 text-center mt-6">
+        {{ title }}
+      </h1>
+      <div
+        class="flex justify-center border w-1/3 m-auto p-2 rounded-full border-blue-800"
+      >
+        <input
+          v-model="newTodo"
+          name=""
+          id=""
+          cols="60"
+          rows="1"
+          placeholder="Enter your task"
+          class="border-none p-2 rounded-lg resize-none focus:outline-none flex-1"
+        />
 
-      <ul>
-        <li v-for="todo in todos" v-bind:key="todo">
-          <input type="checkbox" v-model="todo.done" />
-          <p>{{todo.title}}</p><br>
-          <button @click="removeTodo(todo)" type="button" name="button"><br>
-            Remove
-          </button>
-        </li>
-      </ul>
+        <button
+          @click.prevent="addTodo"
+          class="rounded-full border px-8 py-2 bg-blue-800 text-white"
+        >
+          Submit
+        </button>
+      </div>
+      <div class="w-1/3 m-auto p-4 uppercase text-sm font-semibold border-b">
+        <p>Your To-Do</p>
+      </div>
+
+      <div class="w-1/3 m-auto">
+        <div v-for="todo in todos" v-bind:key="todo">
+          <div class="border-b flex justify-between items-center">
+            <div class="p-4 inline-block">
+              <!-- <input :name="todo.title" type="checkbox" v-model="todo.done" /> -->
+              <div class="text-sm break-all">{{ todo.title }}</div>
+            </div>
+
+            <button
+              style="min-width: 40px"
+              @click="removeTodo(todo)"
+              type="button"
+              name="button"
+              class="rounded-full bg-red-500 hover:bg-red-900 text-white p-2 w-10 h-10 min-w-10"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -40,6 +64,13 @@ export default {
       todos: [],
     };
   },
+  created() {
+    const storagestring = localStorage.getItem("todostore");
+    if (storagestring) {
+      const saved = JSON.parse(storagestring);
+      this.todos = saved;
+    }
+  },
   methods: {
     addTodo() {
       this.todos.push({
@@ -47,10 +78,18 @@ export default {
         done: false,
       });
       this.newTodo = "";
+      const _todo = JSON.stringify(this.todos);
+      console.log(this.todos);
+      console.log(_todo);
+      localStorage.setItem("todostore", _todo);
     },
     removeTodo(todo) {
       const todoIndex = this.todos.indexOf(todo);
       this.todos.splice(todoIndex, 1);
+      const _todo = JSON.stringify(this.todos);
+      
+      localStorage.setItem("todostore", _todo);
+
     },
   },
 };
